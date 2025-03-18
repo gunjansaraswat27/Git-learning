d804109b42c0d3cd5baa7bb76962125669977d9a
Gunjan Saraswat <gunjansarswat9458@gmail.com>
2025-03-18 23:15:13 +0530

    upi Added

--- first.js
+++ first.js
@@ -4,10 +4,14 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
-<<<<<<< HEAD
+
 const footer = "Footer added in our websites";
 console.log(footer);
-=======
+
 const Payment = "Intergrated the payment gateway";
 console.log(Payment);
->>>>>>> 801a43b (Payment Added)
+
+
+const upi = "Integrated the UPI";
+console.log(upi);
+
