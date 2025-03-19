728a10f5b34ba2371b7948bd120d757b3476a655
Gunjan Saraswat <gunjansarswat9458@gmail.com>
2025-03-19 17:50:44 +0530

    Latest Changes

--- first.js
+++ first.js
@@ -4,14 +4,13 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
-
 const footer = "Footer added in our websites";
 console.log(footer);
 
 const Payment = "Intergrated the payment gateway";
 console.log(Payment);
 
-
 const upi = "Integrated the UPI";
 console.log(upi);
 
+console.log("Latest Update");
